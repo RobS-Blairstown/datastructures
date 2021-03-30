e8426e0c8b768bdab612356be31d1352d3414dbb
Rob <rob.sutton123@gmail.com>
2021-03-30 10:30:22 -0400

    almost finished all of problem 1

--- main.py
+++
@@ -1,9 +0,0 @@
-from linkedlist import LinkedList
-
-
-if __name__ == '__main__':
-    linked_list = LinkedList()
-
-    linked_list.append_node(55)
-    linked_list.append_node(60)
-    linked_list.append_node(65)
--- months.py
+++ months.py
@@ -1,7 +1,22 @@
-class Months
+
+
+# problem 1.a
+class Months:
     def __init__(self):
-        self.month = []
+        self.month = {'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'}
 
     def PiDay(pi_day):
         pi_day = "March 14"
-        
+
+#problem 1.b
+    def BdayLocations(self):
+        locations = ['New York', 'LA', 'Florida']
+        for location in locations:
+            print(location)
+
+#problem 1.c
+    def SweepstakesContestants(self):
+        contestants = ('John', 'Larry', 'Tom', 'Annie', 'Barbara')
+        for contestant in contestants:
+            
+
